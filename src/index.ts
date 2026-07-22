a1c7155c4387edd8a94ec8565a937d3bbc598439
TieuGiaBinh <73636576+TieuGiaBinh@users.noreply.github.com>
2026-07-22 11:27:47 +0700

    Initialize Crypto Price Tracker with services and repository

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
 console.log("Crypto Price Tracker started...");
+import { GoogleSheetsRepository } from "./repositories/GoogleSheetsRepository";
+import { PriceTrackerService } from "./services/PriceTrackerService";
+
+const repository = new GoogleSheetsRepository();
+
+const service = new PriceTrackerService(repository);
