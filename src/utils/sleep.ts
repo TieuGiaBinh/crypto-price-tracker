11a1ab9395b4f6e7786c6af1210a4e7a0ea20aff
TieuGiaBinh <73636576+TieuGiaBinh@users.noreply.github.com>
2026-07-24 17:27:46 +0700

    Add sleep function for asynchronous delays

---
+++ src/utils/sleep.ts
@@ -0,0 +1,16 @@
+export async function sleep(
+    milliseconds: number
+): Promise<void> {
+
+    return new Promise(
+
+        resolve =>
+
+            setTimeout(
+                resolve,
+                milliseconds
+            )
+
+    );
+
+}
