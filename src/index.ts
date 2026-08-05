83caaef1a66f9128eb2effb4e0383af1b1b0e461
TieuGiaBinh <73636576+TieuGiaBinh@users.noreply.github.com>
2026-08-05 13:48:39 +0700

    Refactor PriceTrackerService initialization with CoinGeckoClient

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 console.log("Crypto Price Tracker started...");
 import { GoogleSheetsRepository } from "./repositories/GoogleSheetsRepository";
 import { PriceTrackerService } from "./services/PriceTrackerService";
+import { FetchHttpCLient } from "./api/FetchHttpClient"
+import { CoinGeckoClient } from "./api/CoinGeckoClient"
 
 const repository = new GoogleSheetsRepository();
 
-const service = new PriceTrackerService(repository);
+const httpClient = new FetchHttpClient();
+
+const priceProvider = new CoinGeckoClient(httpClient);
+
+const service = new PriceTrackerService(priceProvider, repository);
+
+await service.track("BTC");
