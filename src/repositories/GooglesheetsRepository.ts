558a6be01a8d079cb0fc9814f487137d2fe3acbc
TieuGiaBinh <73636576+TieuGiaBinh@users.noreply.github.com>
2026-08-10 16:05:41 +0700

    Integrate GoogleSheetsClient in GoogleSheetsRepository
    Refactor GoogleSheetsRepository to use GoogleSheetsClient for saving prices.

--- src/repositories/GooglesheetsRepository.ts
+++ src/repositories/GooglesheetsRepository.ts
@@ -1,12 +1,28 @@
 import { CoinPrice } from "../models/CoinPrice";
 import { PriceRepository } from "./PriceRepository";
+import { GoogleSheetsClient } from "../google/GoogleSheetsClient"
 
 export class GoogleSheetsRepository
     implements PriceRepository {
 
+    constructor(
+        private readonly client : GoogleSheetsClient,
+        private readonly speadsheetId : string
+    ){}
+
     async save(price: CoinPrice): Promise<void> {
 
-        console.log(price);
+        await this.client.appendRow(
+
+            this.speadsheetID,
+
+            "Sheet1!A:C",
+            [
+                price.timestamp.toISOSstring(),
+                price.coin,
+                price.price
+            ]
+        );
 
     }
 
